139e097a16d180e99812497e6d1be0f82d70300a
zhuibobo <j.d.r565>
2019-07-04 18:56:20 +0800

    创建项目

---
+++ jb4dc-base-root/jb4dc-base-tools/src/main/java/com/jb4dc/base/tools/FileUtility.java
@@ -0,0 +1,11 @@
+package com.jb4dc.base.tools;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: zhuangrb
+ * Date: 2019/7/4
+ * To change this template use File | Settings | File Templates.
+ */
+public class FileUtility {
+
+}
--- jb4dc-code-general-root/jb4dc-code-generate-web-server/src/main/java/com/jb4dc/code/generate/webserver/ApplicationCodeGenerateWebServer.java
+++ jb4dc-code-general-root/jb4dc-code-generate-web-server/src/main/java/com/jb4dc/code/generate/webserver/ApplicationCodeGenerateWebServer.java
@@ -2,12 +2,19 @@ package com.jb4dc.code.generate.webserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.system.ApplicationHome;
 import org.springframework.context.annotation.ComponentScan;
+
+import java.io.File;
 
 @SpringBootApplication
 @ComponentScan("com.jb4dc")
 public class ApplicationCodeGenerateWebServer {
     public static void main(String[] args) {
         SpringApplication.run(ApplicationCodeGenerateWebServer.class, args);
+
+        ApplicationHome home = new ApplicationHome(ApplicationCodeGenerateWebServer.class);
+        File jarFile = home.getSource();
+        System.out.println("输入出目录:"+jarFile.getParentFile().toString());
     }
 }
