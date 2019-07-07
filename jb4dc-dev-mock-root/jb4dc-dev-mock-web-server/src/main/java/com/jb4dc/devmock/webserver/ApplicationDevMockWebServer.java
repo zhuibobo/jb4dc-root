4c9af5977b0a3a2e345f2144f7f0187e122b1f45
zhuibobo <j.d.r565>
2019-07-07 20:18:19 +0800

    创建项目

--- jb4dc-dev-mock-root/jb4dc-dev-mock-web-server/src/main/java/com/jb4dc/devmock/webserver/ApplicationDevMockWebServer.java
+++ jb4dc-dev-mock-root/jb4dc-dev-mock-web-server/src/main/java/com/jb4dc/devmock/webserver/ApplicationDevMockWebServer.java
@@ -1,10 +1,23 @@
 package com.jb4dc.devmock.webserver;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
 /**
  * Created with IntelliJ IDEA.
  * User: zhuangrb
  * Date: 2019/7/7
  * To change this template use File | Settings | File Templates.
  */
+
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
+@ComponentScan("com.jb4dc")
 public class ApplicationDevMockWebServer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationDevMockWebServer.class, args);
+    }
+
 }
