743c1256221210f2f4dd2580634fc86d1d6d7eb5
zhuibobo <j.d.r565>
2019-11-29 09:26:03 +0800

    创建项目

--- jb4dc-base-root/jb4dc-base-tools/src/main/java/com/jb4dc/base/tools/XMLUtility.java
+++ jb4dc-base-root/jb4dc-base-tools/src/main/java/com/jb4dc/base/tools/XMLUtility.java
@@ -1,10 +1,16 @@
 package com.jb4dc.base.tools;
 
+import com.sun.org.apache.xerces.internal.impl.PropertyManager;
+import com.sun.org.apache.xerces.internal.impl.XMLStreamReaderImpl;
+
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
 import java.io.InputStream;
+import java.io.StringReader;
 import java.io.StringWriter;
 
 public class XMLUtility {
@@ -13,6 +19,15 @@ public class XMLUtility {
         JAXBContext context = JAXBContext.newInstance(_class);
         Unmarshaller unmarshaller = context.createUnmarshaller();
         T vo = (T) unmarshaller.unmarshal(is);
+        return vo;
+    }
+
+    public static <T> T toObject(String xml,Class<T> _class) throws JAXBException, XMLStreamException {
+        JAXBContext context = JAXBContext.newInstance(_class);
+        Unmarshaller unmarshaller = context.createUnmarshaller();
+        StringReader reader = new StringReader(xml);
+        //XMLStreamReader reader1=new XMLStreamReaderImpl(reader, new PropertyManager(PropertyManager.CONTEXT_READER));
+        T vo = (T) unmarshaller.unmarshal(reader);
         return vo;
     }
 
