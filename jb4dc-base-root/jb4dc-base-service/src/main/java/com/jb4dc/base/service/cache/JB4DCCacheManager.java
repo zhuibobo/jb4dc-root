3a3341810f6886ceb0eae3e39fed6670014e8226
zhuibobo <j.d.r565>
2019-12-08 21:38:54 +0800

    创建项目

--- jb4dc-base-root/jb4dc-base-service/src/main/java/com/jb4dc/base/service/cache/JB4DCCacheManager.java
+++ jb4dc-base-root/jb4dc-base-service/src/main/java/com/jb4dc/base/service/cache/JB4DCCacheManager.java
@@ -11,7 +11,7 @@ import org.ehcache.CacheManager;
  * Date: 2018/11/21
  * To change this template use File | Settings | File Templates.
  */
-public class JB4DCCacheManager {
+public abstract class JB4DCCacheManager {
 
     /*public static String jb4dPlatformBuilderCacheName="jb4dPlatformBuilder";
     public static String jb4dPlatformSSOCacheName="jb4dPlatformSSO";
@@ -22,13 +22,21 @@ public class JB4DCCacheManager {
     public static String CACHE_KEY_PROCESS_MODEL_MAIN_IMAGE="JB4DCacheManager.CACHE_KEY_PROCESS_MODEL_MAIN_IMAGE";
     public static String CACHE_KEY_SSO_APP_LOGO="JB4DCacheManager.CACHE_KEY_SSO_APP_LOGO";*/
 
-    private static CacheManager cacheManager = BeanUtility.getBean(CacheManager.class);
+    private CacheManager cacheManager = null;
 
-    public static void put(String cacheName,String key,String value){
+    public JB4DCCacheManager(CacheManager _cacheManager) {
+        cacheManager=_cacheManager;
+    }
+
+    public <K, V> Cache<K,V> getCache(String keyGroup, Class<K> key, Class<V> value){
+        return cacheManager.getCache(keyGroup,key,value);
+    }
+
+    public void put(String cacheName, String key, String value){
         cacheManager.getCache(cacheName,String.class,String.class).put(key,value);
     }
 
-    public static void put(String cacheName,String key,Object value){
+    public void put(String cacheName,String key,Object value){
         cacheManager.getCache(cacheName,String.class,Object.class).put(key,value);
     }
 
@@ -36,15 +44,15 @@ public class JB4DCCacheManager {
         cacheManager.getCache(cacheName,String.class,value.getClass()).put(key,value);
     }*/
 
-    public static <T> T getString(String cacheName,String key){
+    public <T> T getString(String cacheName,String key){
         return (T) cacheManager.getCache(cacheName,String.class,String.class).get(key);
     }
 
-    public static <T> T getObject(String cacheName,String key){
+    public <T> T getObject(String cacheName,String key){
         return (T) cacheManager.getCache(cacheName,String.class,Object.class).get(key);
     }
 
-    public static boolean exist(String cacheName, String key) throws JBuild4DCGenerallyException {
+    public boolean exist(String cacheName, String key) throws JBuild4DCGenerallyException {
         Cache<String, Object> cache = cacheManager.getCache(cacheName, String.class, Object.class);
         if(cache==null){
             throw new JBuild4DCGenerallyException(JBuild4DCGenerallyException.EXCEPTION_PLATFORM_CODE,"不存在名称为:"+cacheName+"的缓存配置!");
@@ -52,17 +60,17 @@ public class JB4DCCacheManager {
         return cache.get(key) != null ? true : false;
     }
 
-    public static <T> T autoGetFromCache(String cacheName,boolean cancelCache,String key,IBuildGeneralObj<T> builder) throws JBuild4DCGenerallyException {
+    public <T> T autoGetFromCache(String cacheName,boolean cancelCache,String key,IBuildGeneralObj<T> builder) throws JBuild4DCGenerallyException {
         T result=null;
         if(cancelCache){
             result=builder.BuildObj();
             return result;
         }
         else{
-            result= JB4DCCacheManager.getObject(cacheName,key);
+            result= this.getObject(cacheName,key);
             if(result==null){
                 result=builder.BuildObj();
-                JB4DCCacheManager.put(cacheName,key,result);
+                this.put(cacheName,key,result);
                 return result;
             }
             return result;
